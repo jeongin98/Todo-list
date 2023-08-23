21c0ace0db80e5c9ee5e0ba7d522755f1a6d3653
jeongin98 <eon7500@gmail.com>
2023-08-23 20:03:45 +0900

    feat: 목록 삭제 기능 구현"

--- main.js
+++ main.js
@@ -3,12 +3,24 @@ const list_Ul = document.getElementById('list_Ul');
 function addList() {
     // 입력 받은 todo 내용
     const inputBoxText = document.getElementById('inputBox').value;
-    // list에 추가
+    // 입력칸이 비어있을 경우 재입력 알림
+    if (inputBoxText == '') { alert("할 일을 입력해주세요"); return; }
+
+    // 입력된 todo 추가
     const newLi = document.createElement('li');
     const newText = document.createTextNode(inputBoxText);
+
+    // Delete 버튼 생성
+    const delButton = document.createElement('button');
+    delButton.setAttribute("onclick", "deleteToDo(this)");
+    delButton.classList.add('deleteToDo');
+    delButton.textContent = "x";
+    // 부모 노드에 자식 노드 연결
     newLi.appendChild(newText);
+    newLi.appendChild(delButton);
     list_Ul.appendChild(newLi);
-    // 입력 후 입력창 지우기
+
+    // 입력 후 입력창 깨끗하게 비우기
     inputBox.value = "";
 }
 
@@ -18,3 +30,10 @@ list_Ul.addEventListener('click', function (event) {
         event.target.classList.toggle('completed');
     }
 });
+
+
+// X표시 버튼 클릭시 todo 삭제
+function deleteToDo(event) {
+    const deleteButton = event; // <Button>
+    deleteButton.parentElement.remove(); // <li>
+}
