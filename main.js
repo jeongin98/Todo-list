e2a03031070f06562591e38e90aa232d92e362c1
jeongin98 <eon7500@gmail.com>
2023-08-23 16:21:36 +0900

    feat: 목록 클릭시 취소선 추가

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const list_Ul = document.getElementById('list_Ul');
 
-// 전역 변수로 바꾸는 방법?
 function addList() {
-
     // 입력 받은 todo 내용
     const inputBoxText = document.getElementById('inputBox').value;
     // list에 추가
@@ -10,9 +8,13 @@ function addList() {
     const newText = document.createTextNode(inputBoxText);
     newLi.appendChild(newText);
     list_Ul.appendChild(newLi);
-
-
-
-
-
+    // 입력 후 입력창 지우기
+    inputBox.value = "";
 }
+
+// 리스트 클릭시 취소선
+list_Ul.addEventListener('click', function (event) {
+    if (event.target.tagName === 'LI') {
+        event.target.classList.toggle('completed');
+    }
+});
